013c34a2c7ceaeaae367e226fae4022ec39ab7aa
Tim Rourke <tim@timrourke.com>
2017-10-29 17:12:21 -0500

    Add unit tests

---
+++ tests/unit/utils/draggable-export-test.js
@@ -0,0 +1,99 @@
+import { test, module } from 'qunit';
+import ShopifyDraggable from '@shopify/draggable';
+
+const {
+  BaseEvent,
+  Collidable,
+  Draggable,
+  Droppable,
+  ForceTouchSensor,
+  Snappable,
+  Sortable,
+  Swappable,
+} = ShopifyDraggable;
+
+module('Unit | Draggable exports');
+
+test('ShopifyDraggable exports', function(assert) {
+  assert.ok(ShopifyDraggable, 'Draggable exports an object');
+});
+
+test('Draggable class exports', function(assert) {
+  assert.equal(typeof Draggable, 'function');
+});
+
+test('Draggable() instance of Draggable', function(assert) {
+  const instance = new Draggable();
+
+  assert.ok(instance instanceof Draggable);
+});
+
+test('Droppable class exports', function(assert) {
+  assert.equal(typeof Droppable, 'function');
+});
+
+test('Droppable() instance of Droppable', function(assert) {
+  const instance = new Droppable();
+
+  assert.ok(instance instanceof Droppable);
+});
+
+test('BaseEvent class exports', function(assert) {
+  assert.equal(typeof BaseEvent, 'function');
+});
+
+test('BaseEvent() instance of BaseEvent', function(assert) {
+  const instance = new BaseEvent();
+
+  assert.ok(instance instanceof BaseEvent);
+});
+
+test('Collidable class exports', function(assert) {
+  assert.equal(typeof Collidable, 'function');
+});
+
+test('Collidable() instance of Collidable', function(assert) {
+  const instance = new Collidable();
+
+  assert.ok(instance instanceof Collidable);
+});
+
+test('ForceTouchSensor class exports', function(assert) {
+  assert.equal(typeof ForceTouchSensor, 'function');
+});
+
+test('ForceTouchSensor() instance of ForceTouchSensor', function(assert) {
+  const instance = new ForceTouchSensor();
+
+  assert.ok(instance instanceof ForceTouchSensor);
+});
+
+test('Snappable class exports', function(assert) {
+  assert.equal(typeof Snappable, 'function');
+});
+
+test('Snappable() instance of Snappable', function(assert) {
+  const instance = new Snappable();
+
+  assert.ok(instance instanceof Snappable);
+});
+
+test('Sortable class exports', function(assert) {
+  assert.equal(typeof Sortable, 'function');
+});
+
+test('Sortable() instance of Sortable', function(assert) {
+  const instance = new Sortable();
+
+  assert.ok(instance instanceof Sortable);
+});
+
+test('Swappable class exports', function(assert) {
+  assert.equal(typeof Swappable, 'function');
+});
+
+test('Swappable() instance of Swappable', function(assert) {
+  const instance = new Swappable();
+
+  assert.ok(instance instanceof Swappable);
+});
