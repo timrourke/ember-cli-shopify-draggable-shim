5941dbd221afd6203f03864a714b210581001d13
Adam Anderson <3806+scudco@users.noreply.github.com>
2018-09-24 17:38:54 -0400

    Add fastboot support as documented on [ember-fastboot](1)
    [1]: https://github.com/ember-fastboot/ember-cli-fastboot#build-hooks-for-fastboot

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var path = require('path');
 var Funnel = require('broccoli-funnel');
 var MergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
 
 module.exports = {
   name: 'ember-cli-shopify-draggable',
@@ -15,11 +16,12 @@ module.exports = {
   },
 
   treeForVendor(vendorTree) {
-		var draggablePath = path.dirname(require.resolve('@shopify/draggable/lib/draggable.bundle.js'));
-		var draggableTree = new Funnel(draggablePath, {
+    var draggablePath = path.dirname(require.resolve('@shopify/draggable/lib/draggable.bundle.js'));
+    var draggableTree = new Funnel(draggablePath, {
       files: ['draggable.bundle.js']
     });
+    var fastbootSafeDraggableTree = map(draggableTree, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
 
-    return new MergeTrees([vendorTree, draggableTree]);
+    return new MergeTrees([vendorTree, fastbootSafeDraggableTree]);
   },
 };
